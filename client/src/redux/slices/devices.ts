895f5dabb39d727a3498e462aeee84f71e12b948
yututi <y.tsuchiya0207@gmail.com>
2021-06-13 23:20:17 +0900

    カメラのない端末でエラーになっていたので修正

--- client/src/redux/slices/devices.ts
+++ client/src/redux/slices/devices.ts
@@ -86,10 +86,10 @@ export const slice = createSlice({
       }
       // この辺の、デフォルトデバイスを決定するやり方を改善する必要あり
       if (payload.videos.every(video => video.deviceId !== state.current.videoId)) {
-        state.current.videoId = payload.videos[0].deviceId
+        state.current.videoId = payload.videos[0]?.deviceId
       }
       if (payload.audioIns.every(audio => audio.deviceId !== state.current.audioInId)) {
-        state.current.audioInId = payload.audioIns[0].deviceId
+        state.current.audioInId = payload.audioIns[0]?.deviceId
       }
 
     },
